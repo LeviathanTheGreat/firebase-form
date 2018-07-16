5e217122bbe07613ee958aa13a217a85d8c69fe6
Chris Cormier <chriscormier00@gmail.com>
2018-07-16 15:46:51 -0300

    updated file

--- js/card-gallery[react].js
+++ js/card-gallery[react].js
@@ -1,29 +1,45 @@
 class CardGallery extends React.Component{
     constructor(props){
-        this.props = props;
+        
+        this.props = { 
+            cards:[
+            /*
+                name:,
+                sex:,
+                createdBy:, 
+            */    
+            ]
+        };
+        
         this.state = { numOfChildren:0 };    
     }
-    
-    
-    props = { 
-        cards:{
-        /*
-            name:,
-            sex:,
-        */    
-        }
-    };
+
     static addCard(snapshot){
         snapshot.val()
+        numOfChildren++
     }
     
+    static removeCardWrapper(){
+    
+    }
+    
+    static removeCard(arrayIndex,owner){
+        firebase.database().ref("/dogs")
+        
+    }
     
     
     
     render(){
+        let cards = 
         return (
-            <div class="" >
-        
+            <div class="gallery" >
+                <div class="outer_frame">
+                    <div></div>
+                </div>
+            </div>
+            <div class="showmore">
+                    
             </div>
         )
     }
