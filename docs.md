4788da26fb244d4542ceb25624aaba6a94a473b9
Chris Cormier <chriscormier00@gmail.com>
2018-07-17 22:21:23 -0300

    fixed tests to the point that they should be working

---
+++ docs.md
@@ -0,0 +1,15 @@
+
+
+file loading order 
+
+tinyColor2, -> main.js -> react -> firebase-app, firebase-database, firebase event listeners 
+
+
+worries
+
+-firebase(obvious) and the best ways to interact with the react gallery component
+-react method setState
+-imports
+-adding v-for mechanism for react rendering
+
+
---
+++ tests/firebase-listeners-tests.js
@@ -0,0 +1,82 @@
+const mocha = require("mocha");
+const {assert, expect, should} = require("chai"); 
+const nodeAssert = require("assert")
+const sinon = require("sinon");
+const fakeData = JSON.parse(require("test-dependancies/firebase-fake_data.js"));
+
+mocha.setup("tdd")
+
+describe("dependancies loaded",function(){
+    
+    it("fakeData is present",function(){
+        assert.ok(fakeData)       
+    })
+
+    it("example data is an object",function(){
+        assert(typeof fakeData === "object")
+    })
+
+
+    it("tests global presence of chai assert",function(){
+        nodeAssert(expect)       
+    })
+    it("tests global presence of chai assert",function(){
+        nodeAssert(assert)
+    })
+    it("tests global presence of chai should",function(){
+        nodeAssert(should)
+    })
+
+})
+
+describe("testing the lib. functions",function(){
+    
+});
+
+describe("tests value response of child_added listener", function(){
+    
+    beforeEach(function(){
+        for(let i = 0; i < fakeData; ){
+            firebase.database().ref("/testDogs").push({
+                name:"sam",
+                sex:"is maleish",
+                hair-color:"brownish",
+                description:"is doggish",
+            
+            }) 
+        }
+    })
+    
+    
+    
+    before(function(){
+        var spyHandler = sinon.spy();    
+    })
+        
+    it("the event fired, the mocha before works sorta as like extending the scope,and the value was picked up",function(){
+        
+        
+        firebase.on("child_added", function(snapshot){
+            let Fval = snapshot.val();
+            console.log(val)  
+            spyHandler(Fval)
+        })
+
+        assert(spyHandler.calledWith)
+    })
+
+
+
+    afterEach(function(){
+        firebase.database().ref("/testDogs").parent()/* ->   */
+    })
+    
+    afterEach("take away the firebase event listner",function(){
+        firebase.database().ref("/testDogs").off("child_added")
+    })
+    
+    
+})
+
+
+mocha.run()
