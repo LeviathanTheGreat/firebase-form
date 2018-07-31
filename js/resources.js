3f3903c2d73e1f0a56f137adbb793f0e2a08097b
Chris Cormier <chriscormier00@gmail.com>
2018-07-31 20:32:22 -0300

    overhauled structure and made into module

--- js/resources.js
+++ js/resources.js
@@ -1,51 +1,50 @@
-const resources = {
-  elemsToSubmit : document.getElementsByClassName(".toSubmit"),
-  sendableNode: function(node){
-    switch(node.tagName){
-      case "INPUT":
-          if(node.value.length > 0){
-            return false;
-          }
-          return true;
-      case "TEXTAREA":
-          return true;
-      case "FORM":
-          return true;
-      default:
-          return false;
-    }
-  }
-};
-
-function attachButtonChangeListener(nodesToUpdate){
-	firebase.database().ref("/").child("button color").on("value", (data)=>{
-	    const objectValue = data.val();
-	    if(typeof objectValue === "string"){
-	    	nodesToUpdate["button"].style.backgroundColor = objectValue; 
+module.exports = {
+	  isSendableNode: function (node){
+	    switch(node.tagName){
+	      case "INPUT":
+		  if(node.value.length > 0){
+		    return false;
+		  }
+		  return true;
+	      case "TEXTAREA":
+		  return true;
+	      case "FORM":
+		  return true;
+	      default:
+		  return false;
 	    }
-	}, (errorObject) => {
-		console.error("there was a problem ");
-	    throw errorObject;
-	})
-}
+	  }
+	},
+
+	attachButtonChangeListener: function(nodesToUpdate){
+		firebase.database().ref("/").child("button color").on("value", (data)=>{
+		    const objectValue = data.val();
+		    if(typeof objectValue === "string"){
+			nodesToUpdate["button"].style.backgroundColor = objectValue; 
+		    }
+		}, errorObject =>{
+		    throw errorObject;
+		})
+	},
 
 
 
 
-function changeButtonColor(btn){
-    tinyColor ? console.log() : throw new Error("tinycolor dependantcy not loaded");    
-    function randRGBValue(){
-        return Math.floor(Math.random() * 255)
-    }
-    let randRGB = {
-        r:randRGBValue(),
-        g:randRGBValue(),
-        b:randRGBValue()
-    }
-    
-    let tinyColorObject = tinyColor(randRGB);
-    let color = tinyColorObject.name();
-    
+	changeButtonColor : function(btn){
+	    tinyColor ? console.log() : throw new Error("tinycolor dependantcy not loaded");    
+	    function randRGBValue(){
+		return Math.floor(Math.random() * 255)
+	    }
+	    let randRGB = {
+		r:randRGBValue(),
+		g:randRGBValue(),
+		b:randRGBValue()
+	    }
 
-    firebase.database().ref().child("button color").set(color);
-}
+	    let tinyColorObject = tinyColor(randRGB);
+	    let color = tinyColorObject.name();
+
+
+	    firebase.database().ref().child("button color").set(color);
+	},
+};
