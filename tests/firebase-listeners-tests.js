b26dff66486a49179f5e2fc9dec8c54f8b558a93
Chris Cormier <chriscormier00@gmail.com>
2018-08-01 09:33:07 -0300

    added config

--- tests/firebase-listeners-tests.js
+++ tests/firebase-listeners-tests.js
@@ -3,16 +3,19 @@ const {assert, expect, should} = require("chai");
 const nodeAssert = require("assert")
 const sinon = require("sinon");
 const firebase = require("firebase");
-const fakeData = JSON.parse(require("test-dependancies/firebase-fake_data.js"));
+const fakeDoggoData = JSON.parse(require("test-dependancies/firebase-fake_data.js"));
 
 //database initialization
 let config = {
-    
+    apiKey: "AIzaSyCTzXVtnpGNABDoG1brrT_nrNmx_JBglsI",
+    authDomain: "my-test-project-fcd05.firebaseapp.com",
+    databaseURL: "https://my-test-project-fcd05.firebaseio.com",
+    projectId: "my-test-project-fcd05",
+    storageBucket: "my-test-project-fcd05.appspot.com",
+    messagingSenderId: "862753541155"
 };
 
 firebase.initializeApp(config)
-
-
 
 mocha.setup("tdd")
 
@@ -41,39 +44,37 @@ describe("testing the lib functions",function(){
 describe("tests value response of child_added listener", function(){
     
     beforeEach(function(){
-        for(let i = 0; i < fakeData; ){
+        for(let i = 0; i < fakeDoggoData.length; ){
             firebase.database().ref("/testDogs").push({
                 name:"sam",
                 sex:"is maleish",
                 hair-color:"brownish",
                 description:"is doggish",
             
-            }) 
+            })
         }
     })
     
-    
-    
-    before(function(){
-        var spyHandler = sinon.spy();    
-    })
+
         
     it("the event fired, the mocha before works sorta as like extending the scope,and the value was picked up",function(){
         
         
+        
         firebase.on("child_added", function(snapshot){
             let Fval = snapshot.val();
-            console.log(val)  
-            spyHandler(Fval)
+            assert.ok(Fval)
         })
+            
 
-        assert(spyHandler.calledWith)
     })
-
-
-
+    
+    it("tests that the firebase push funtionality is working",function(){
+            
+    });
+    
     afterEach(function(){
-        firebase.database().ref("/testDogs").parent()/* ->   */
+        firebase.database().ref("/testDogs").removeValue()/* ->   */
     })
     
     afterEach("take away the firebase event listner",function(){
