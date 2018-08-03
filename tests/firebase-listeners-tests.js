7bdb952e07a7415ecf5482e57104045e9deac9e1
Chris Cormier <chriscormier00@gmail.com>
2018-08-03 11:26:24 -0300

    added tests for push functionality

--- tests/firebase-listeners-tests.js
+++ tests/firebase-listeners-tests.js
@@ -21,8 +21,8 @@ mocha.setup("tdd")
 
 describe("dependancies loaded",function(){
     
-    it("fakeData is present",function(){
-        assert.ok(fakeData)       
+    it("test that the fakeData dep. is loaded for default doggos  ",function(){
+        assert.ok(fakedData)       
     })
 
     it("tests global presence of chai assert",function(){
@@ -44,13 +44,9 @@ describe("testing the lib functions",function(){
 describe("tests value response of child_added listener", function(){
     
     beforeEach(function(){
-        for(let i = 0; i < fakeDoggoData.length; ){
+        for(let i = 0; i < fakeDoggoData.length; i++ ){
             firebase.database().ref("/testDogs").push({
-                name:"sam",
-                sex:"is maleish",
-                hair-color:"brownish",
-                description:"is doggish",
-            
+                fakeDoggoData[i]...
             })
         }
     })
@@ -59,28 +55,44 @@ describe("tests value response of child_added listener", function(){
         
     it("the event fired, the mocha before works sorta as like extending the scope,and the value was picked up",function(){
         
+        //let mock = 
         
         
         firebase.on("child_added", function(snapshot){
             let Fval = snapshot.val();
             assert.ok(Fval)
+            //assert.calledWith()
         })
             
 
     })
     
     it("tests that the firebase push funtionality is working",function(){
-            
+        
+        let doggoAdded;
+        
+        firebase.on("child_added", function(snapshot){
+            let doggoAdded = snapshot.val();
+        })
+        
+         firebase.database().ref("/testDogs").push({
+                name:"Sam",
+                sex:"is maleish",
+                hairColor:"brownish",
+                description:"is doggish",
+         })
+        
+        assert(doggoAdded["name"] === "sam") 
+   
     });
     
     afterEach(function(){
-        firebase.database().ref("/testDogs").removeValue()/* ->   */
+        firebase.database().ref("/testDogs").removeValue()
     })
     
-    afterEach("take away the firebase event listner",function(){
+    afterEach(function(){
         firebase.database().ref("/testDogs").off("child_added")
     })
-    
     
 })
 
