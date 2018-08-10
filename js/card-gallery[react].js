fd2d1df35e886b447a870e6fa811bec43f3e6d8b
Chris Cormier <chriscormier00@gmail.com>
2018-08-09 21:38:36 -0300

    forgot to save edit to react components contents, specifically render function contents
    added mounting command, removed props handling from component

--- js/card-gallery[react].js
+++ js/card-gallery[react].js
@@ -6,36 +6,35 @@ import default as "firebaseSetup" from "./firebaseListeners"
 class CardGallery extends React.Component{
     constructor(props){
         super(props)
-        this.props = { 
-            cards:[
-            /*
-                name:,
-                sex:,
-                createdBy:, 
-            */    
-            ]
-            this.state = {
-                numOfCards:0, 
-                cards:this.props.cards, 
-            };
+        this.state = { 
+                cards:[]
         };
-        this.galleryStyle = " display:block; 
+        this.galleryStyle = ` display:block; 
                               width:auto;
                               height:auto;
-
 
                               background-color:white;
                               border-radius:25px;  
 
-
-                                    "; 
+                                                    `; 
             
     }
 
+    componentWillMount(){
+        const that = this;
+        firebase.database().ref("/dogs").on("child_added",(snapshot)=>{
+            
+            that.state.cards.push(snapshot.val());            
+        })
+    }
+    
     static addCard(dogAttributes){
         //pushes an object with all dog
-        firebase.database().ref("/dogs").push(dogAttributes).then(()=>{
+        firebase.database().ref("/dogs").push(dogAttributes)
+        .then(()=>{
             this.setState(/*???*/)
+        }).catch((e)=>{
+            "dog could not be added to database"
         })
     }
     
@@ -43,22 +42,29 @@ class CardGallery extends React.Component{
         firebase.database().ref("/dogs").find().remove()
         //updates in 
         .then(()=>{
-            numOfCards--;
             this.setState(/*???*/)    
+        }).catch(()=>{
+            
         }) //(?)
         
     }
      
     render(){
+        let cards = this.state.cards; 
         const cardsToRender = this.props.cards.forEach((cardInfo)=>(<gallery-card>))
+        
         return (
             <div class="gallery" >
-                <div class="outer_frame" style="">
-                    <gallery-card  />
+                <div class="outer_frame">
+                    {                                
+                        cards.map((card)=>{    
+                            
+                        }) 
+                    }
                 </div>
             </div>
             <div class="ui button secondary show_more">
-                        
+                <i> </i>            
             </div>
         )
     }
