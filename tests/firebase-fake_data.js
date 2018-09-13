871b9cffd2bd4bf0e702cdd083f31673bb1f34d0
Chris Cormier <chriscormier00@gmail.com>
2018-09-13 16:49:37 -0300

    update fake information

--- tests/firebase-fake_data.js
+++ tests/firebase-fake_data.js
@@ -2,19 +2,22 @@ module.exports = [
 	{
 		name:"sam",
 		breed:"terrier",
+		sex:"male",
 		hairColor:"brown",
 		description:"he is such a good boi",
 	},
 	{
-		name:"sam",
-		breed:"terrier",
+		name:"tabatha",
+		breed:"rotweiller",
+		sex:"female",
 		hairColor:"brown",
 		description:"he is such a good boi",
 	},
 	{
-		name:"sam",
-		breed:"terrier",
-		hairColor:"brown",
+		name:"august",
+		breed:"shelty",
+		sex:"female",
+		hairColor:"golden",
 		description:"he is such a good boi",
 	},
 ];
